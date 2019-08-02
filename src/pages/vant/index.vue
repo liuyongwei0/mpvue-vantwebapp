e9591a8ba01040bd41c690b0609ca486a7ad0ff6
DESKTOP-MPTH0DG\LYW <liuyongwei@blchina.com>
2019-08-02 17:49:13 +0800

    新建项目

---
+++ src/pages/vant/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="counter-warp">
+      fuck you
+  </div>
+</template>
+
+<script>
+// Use Vuex
+import store from './store'
+
+export default {
+  computed: {
+    count () {
+    }
+  },
+  methods: {
+    increment () {
+    },
+    decrement () {
+    }
+  }
+}
+</script>
+
+<style>
+.counter-warp {
+  text-align: center;
+  margin-top: 100px;
+}
+.home {
+  display: inline-block;
+  margin: 100px auto;
+  padding: 5px 10px;
+  color: blue;
+  border: 1px solid blue;
+}
+</style>

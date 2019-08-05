11fe145f81f1916645a62cea1bd086511831601d
DESKTOP-MPTH0DG\LYW <liuyongwei@blchina.com>
2019-08-05 09:51:09 +0800

    新增vant-weapp

--- src/pages/vant/index.vue
+++ src/pages/vant/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="counter-warp">
-      fuck you
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
 
 <script>
 // Use Vuex
-import store from './store'
 
 export default {
   computed: {
